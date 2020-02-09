3da2f64ec846c3b47fd03e173831c85c1b96e110
João Paulo Buchholz <51836804+Jompa14@users.noreply.github.com>
2020-02-09 20:33:40 -0300

    storege user name
    storege user name end prevent the contenteditable feature to create a <br> when pressure enter

--- matrix.js
+++ matrix.js
@@ -13,7 +13,7 @@ function showTime() {
   }
   
 //    Add Zeros - fixing a problem of shouTimeout()
-//   who displays the our like this-> 22:9.
+//   who displays the hour like this-> 22:9.
 //   the function below add a zero when it's needed.
   function addZero(n) {
     return (parseInt(n, 10) < 10 ? '0' : '') + n;
@@ -28,6 +28,32 @@ var day = today.toLocaleDateString("en-US", options);
   document.getElementById("date").innerHTML = day;
 }
 
+//prevent Enter to create a break at contenteditable
+document.getElementById('name').addEventListener('keydown', (evt) => {
+  if (evt.keyCode === 13) {
+      evt.preventDefault();
+  }
+});
+
+
+// save edits at localStorage
+const editables = document.querySelectorAll("[contenteditable]");
+
+editables.forEach(el => {
+  el.addEventListener("blur", () => {
+    localStorage.setItem("dataStorage-" + el.id, el.innerHTML);
+  })
+});
+
+// once on load
+for (var key in localStorage) {
+  if (key.includes("dataStorage-")) {
+    const id = key.replace("dataStorage-","");
+    document.querySelector("#" + id).innerHTML = localStorage.getItem(key);
+  }
+}
+
 //Run the functions
   showTime();
   displaytDate();
+  setName();
